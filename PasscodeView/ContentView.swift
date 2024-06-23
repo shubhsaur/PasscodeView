22d9566d06c0a7e59a11aa85abb72115f76c63cc
shubhsaur <shubhamsaurabh@outlook.com>
2024-06-23 20:00:54 +0530

    Initial Commit

---
+++ PasscodeView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PasscodeView
+//
+//  Created by Shubham Saurabh on 23/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PasscodeView/PasscodeViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasscodeViewApp.swift
+//  PasscodeView
+//
+//  Created by Shubham Saurabh on 23/06/24.
+//
+
+import SwiftUI
+
+@main
+struct PasscodeViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
